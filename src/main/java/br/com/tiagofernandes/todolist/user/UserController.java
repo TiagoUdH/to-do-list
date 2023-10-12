716d7fa2f3f57a27f873ac35acb797b70addae35
Tiago Fernandes <tiagoferprojects@gmail.com>
2023-10-11 23:55:47 -0600

    Implementing User Authentication

--- src/main/java/br/com/tiagofernandes/todolist/user/UserController.java
+++ src/main/java/br/com/tiagofernandes/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -16,12 +18,15 @@ public class UserController {
   private IUserRepository userRepository;
 
   @PostMapping("/")
-  public ResponseEntity create(@RequestBody UserModel userModel) {
+  public ResponseEntity<?> create(@RequestBody UserModel userModel) {
     var user = this.userRepository.findByUsername(userModel.getUsername());
 
     if (user != null) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
     }
+
+    var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+    userModel.setPassword(passwordHashred);
 
     var userCreated = this.userRepository.save(userModel);
 
